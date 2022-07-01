a7ae820b5c6a53f086523a0c042cbcb58254b59e
Ahmadali Ghaffari <ahmadali.ghaffari@gmail.com>
2022-07-01 21:49:59 +0200

    some refactoring of the files

--- contract-interact.js
+++ contract-interact.js
@@ -4,6 +4,9 @@ const web3 = new Web3(new Web3.providers.HttpProvider("https://kovan.infura.io/v
 const CONTRACT_ADDRESS = "0x365441EC0974F6AC9871c704128e9da2BEdE10CE";
 const API_KEY = "854HHTC4MKJAJWY9TK1FZYZ7NY3IJMKTDH";
 
+const container = document.querySelector(".container");
+const openDLCCountSpan = document.querySelector(".open-dlc-count");
+
 if (!web3.utils.isAddress(CONTRACT_ADDRESS)) {
   console.log("Not Valid Contract Address");
 };
@@ -13,29 +16,22 @@ axios.get(`https://api-kovan.etherscan.io/api?module=contract&action=getabi&addr
     var contractABI = "";
     contractABI = JSON.parse(response.data.result);
     if (contractABI != '') {
-      // console.log(contractABI[22])
-      console.log("\n\n")
-      const contractDetails = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS)
-
-      // contractDetails.methods.symbol().call({ from: CONTRACT_ADDRESS }, function (error, result) {
-      //   console.log("Result of symbol: " + result)
-      // });
-
-
-
+      // console.log(contractABI[22]);
+      console.log("\n\n");
+      const contractDetails = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS);
 
       console.log(contractDetails.methods, '============> ALL THE METHODS');
-
       console.log(Math.floor(Date.now() / 1000), 'CURRENT TIME!');
-      contractDetails.methods.requestCreateDLC(CONTRACT_ADDRESS, Math.floor(Date.now() / 1000) + 2,1656698460 );
 
-      // contractDetails.methods.isMintedBeforeReveal(1).call({ from: CONTRACT_ADDRESS }, function (error, result) {
-      //   console.log("Result of isMintedBeforeReveal: " + result)
-      // });
+      contractDetails.methods.allOpenDLC().call({ from: CONTRACT_ADDRESS }, function (error, result) {
+        console.log("OPEN DLC's: ", result.length, Array.isArray(result));
+        openDLCCountSpan.textContent = `The number of open DLC = ${result.length}`;
+      });
 
-      //console.log('This is the contract Details: ==>', contractDetails);
-    }
-    else {
+      contractDetails.methods.requestCreateDLC(CONTRACT_ADDRESS, 1656700649, 1656700348).call({ from: CONTRACT_ADDRESS }, function (error, result) {
+        console.log("Result of symbol: " + result);
+      });
+    } else {
       console.log(`Error fetching ABI contract for address ${CONTRACT_ADDRESS}`);
     }
   })
@@ -43,13 +39,8 @@ axios.get(`https://api-kovan.etherscan.io/api?module=contract&action=getabi&addr
     console.log(error);
   });
 
-
-// const API_KEY = process.env.API_KEY;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY;
-// const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const btn = document.querySelector('button');
 
 btn.onclick = () => {
   console.log('hello');
 };
-
--- dlc-manager.js
+++ dlc-manager.js
@@ -7,10 +7,13 @@ window.onload = () => {
       let unix_timestamp = Number(allDLC[allDLC.length - 1].timeStamp);
       let date = new Date(unix_timestamp * 1000);
       let dateSpan = document.createElement('span');
-      dateSpan.className =
-        dateSpan.textContent = ` The age of the newest DLC: ` + date;
+      dateSpan.className = 'date';
+      dateSpan.textContent = ` The age of the newest DLC: ` + date;
       const containerDiv = document.querySelector('.container');
       const dlcCount = document.createElement('span');
+      const openDLCCountSpan = document.createElement('span');
+      openDLCCountSpan.className = 'open-dlc-count';
+
       const newHr = document.createElement('hr');
       const newHr2 = document.createElement('hr');
       const dlcCountParagraph = document.createElement('p');
@@ -23,15 +26,16 @@ window.onload = () => {
       containerDiv.appendChild(newHr);
       containerDiv.appendChild(dateSpan);
       containerDiv.appendChild(newHr2);
+
       for (let i = allDLC.length - 1; i >= 0; i--) {
         const newUl = document.createElement('ul');
         const newLi = document.createElement('li');
         newLi.className = 'dlc-details';
-        newLi.innerText = `DLC ${i + 1} - blockNumber: ${allDLC[i].blockNumber}, timeStamp: ${allDLC[i].timeStamp}, gasUsed: ${allDLC[i].gasUsed}, nonce: ${allDLC[i].nonce}, 
-        blockHash: ${allDLC[i].blockHash}`;
-        containerDiv.appendChild(newLi);
+        newLi.innerText = `DLC ${i + 1} - blockNumber: ${allDLC[i].blockNumber}, timeStamp: ${allDLC[i].timeStamp},
+         gasUsed: ${allDLC[i].gasUsed}, nonce: ${allDLC[i].nonce}, blockHash: ${allDLC[i].blockHash}`;
+        newUl.appendChild(newLi);
+        containerDiv.appendChild(newUl);
       };
-
     })
     .catch(err => console.log('rejected', err.message));
 };
@@ -42,17 +46,10 @@ const getAllDLC = async () => {
       `https://api-kovan.etherscan.io/api?module=account&action=txlist&address=0x365441EC0974F6AC9871c704128e9da2BEdE10CE
     &startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey=854HHTC4MKJAJWY9TK1FZYZ7NY3IJMKTDH`
     );
+
   if (response.status !== 200) {
     throw new Error('Cannot fetch the data!');
   };
   const data = await response.json();
   return data;
 };
-
-// btn.addEventListener('click', () => {
-//   getJokes()
-//     .then(data => {
-
-//     })
-//     .catch(err => console.log('rejected', err.message));
-// });
